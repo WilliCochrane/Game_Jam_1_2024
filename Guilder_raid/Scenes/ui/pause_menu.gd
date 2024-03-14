bc4b2020db55afac9c23c39d5ccdd8a1c9540764
WilliCochrane <iliketobuyhats@gmail.com>
2024-03-14 14:40:06 +1300

    pause menu

--- Guilder_raid/Scenes/ui/pause_menu.gd
+++ Guilder_raid/Scenes/ui/pause_menu.gd
@@ -4,14 +4,20 @@ signal opened
 signal closed
 
 @onready var ability_display : Control = $Ability_display
+@export var canvas_modulate : CanvasModulate
 
 var is_open : bool = false
+
+func _ready():
+	$Gama_Slider.value = canvas_modulate.color.r/100
+
 
 func open():
 	visible = true
 	is_open = true
 	opened.emit()
 	get_tree().paused = true
+
 
 func close():
 	visible = false
@@ -22,3 +28,7 @@ func close():
 
 func _on_shop_shop_closed():
 	ability_display.update()
+
+
+func _on_h_slider_value_changed(value):
+	canvas_modulate.color = Color(value/100,value/100,value/100)
